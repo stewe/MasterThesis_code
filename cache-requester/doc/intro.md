abd3e590b2582570091dcf5fb2c70835ac681760
Stefan Wendt <stefanwendt@posteo.de>
2015-11-18 19:39:53 +0100

    init a simple requester (clojure) for the cache api

---
+++ cache-requester/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to cache-requester
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ cache-requester/src/cache_requester/core.clj
@@ -0,0 +1,19 @@
+(ns cache-requester.core
+  (:gen-class)
+  (:require [zeromq.zmq :as zmq]))
+
+  (def addr "tcp://127.0.0.1:5550")
+  (def req-get "get INF (sensor-java, a, 3)")
+
+  ; TODO think about cache API get:
+  ; GET duration filter1 amount1 filter2 amount2 ...
+  ; now: one queue per sensor ->
+
+  (defn -main []
+    (let [context (zmq/context 1)]
+      (println "Connecting to the cache…")
+      (with-open [socket (doto (zmq/socket context :req)
+                                  (zmq/connect addr))]
+                          (println "Sending " req-get "…")
+                          (zmq/send-str socket req-get)
+                          (println "Received " (String. (zmq/receive socket))))))
