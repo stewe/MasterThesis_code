6f774542a0af622b7e075a4237a19c52fa8e67a3
Stefan Wendt <stefanwendt@posteo.de>
2016-05-08 18:41:26 +0200

    REMOVING pub/sub functionality; Cache uses a slab as a placeholder for unprotected mem

---
+++ cache/src/.cache_ds.rs
@@ -0,0 +1,182 @@
+/// heavily inspired by Crate lru_time_cache
+
+extern crate time;
+
+use std::collections::{HashMap, HashSet, VecDeque};
+use std::hash::Hash;
+use std::mem::size_of_val;
+use time::{Duration, get_time, Timespec};
+
+pub struct CacheDS<K, V> {
+    map: HashMap<K, (V, Option<Timespec>)>,
+    list: VecDeque<K>,
+}
+
+impl<K, V> CacheDS<K, V> where K: PartialOrd + Ord + Clone + Hash {
+    pub fn new() -> CacheDS<K, V> {
+        CacheDS {
+            map: HashMap::new(),
+            list: VecDeque::new(),
+        }
+    }
+
+    fn insert(&mut self, key: K, value: V, expiry: Option<Timespec>) -> Option<V> {
+        let result = self.remove(&key);
+        // TODO delete expired values, check for size and in case remove with lru
+        self.remove_expired();
+        // while not_enough_space {
+        //     self.remove_lru();
+        // }
+
+
+        self.map.insert(key.clone(), (value, expiry));
+        self.list.push_back(key);
+        result
+
+    }
+
+    pub fn insert_with_expiry(&mut self, key: K, value: V, expiry: Timespec) -> Option<V> {
+        self.insert(key, value, Some(expiry))
+    }
+
+    pub fn insert_with_ttl(&mut self, key: K, value: V, ttl: i64) -> Option<V> {
+        let expiry = get_time() + Duration::milliseconds(ttl);
+        self.insert(key, value, Some(expiry))
+    }
+
+    pub fn insert_without_expiry(&mut self, key: K, value: V) -> Option<V> {
+        self.insert(key, value, None)
+    }
+
+    pub fn remove(&mut self, key: &K) -> Option<V> {
+        if self.contains_key(key) {
+            // self.list.retain(|k| k < key || k > key); // iterating all elements is necassary when keys can be contained multiple times
+            Self::find_and_remove(&mut self.list, key);
+            self.map.remove(key).map(|(value, _)| value)
+        } else {
+            None
+        }
+    }
+
+    pub fn contains_key(&self, key: &K) -> bool {
+        self.map.contains_key(key)
+    }
+
+    fn find_and_remove(list: &mut VecDeque<K>, key: &K) {
+        match list.iter().position(|k| !(k < key || k > key)) {
+            Some(index) => { list.remove(index); },
+            None => { },
+        }
+    }
+
+    fn is_expired(value: &(V, Option<Timespec>), now: Timespec) -> bool {
+        match value.1 {
+            None => false,
+            Some(expiry) => expiry <= now,
+        }
+    }
+
+    /// Since get has to be fast, the removal of expired value isn't triggered here.
+    pub fn get(&mut self, key: &K) -> Option<&V> {
+        let result = self.map.get(key);
+        match result {
+            Some(x) if !Self::is_expired(x, get_time()) => {
+                Self::find_and_remove(&mut self.list, key);
+                self.list.push_back(key.clone());
+                Some(&x.0)
+            },
+            _ => None,
+        }
+    }
+
+    fn remove_lru(&mut self) {
+        match self.list.pop_front() {
+            Some(key) => {
+                self.map.remove(&key);
+            },
+            None => {},
+        }
+    }
+
+    pub fn remove_expired(&mut self) {
+        let now = get_time();
+        let mut expired = HashSet::new();
+        for (k, v) in self.map.iter() {
+            if Self::is_expired(v, now) { expired.insert(k.clone()); }
+        }
+        for key in expired.clone() {
+            self.map.remove(&key);
+        }
+        self.map.shrink_to_fit();
+        self.list.retain(|k| !expired.contains(k));
+        self.list.shrink_to_fit();
+    }
+
+    pub fn get_mem_size(&self) -> usize {
+        let size = size_of_val(self);
+        let size_map = size_of_val(&self.map);
+        let size_list = size_of_val(&self.list);
+        println!("Size of CacheDS: {}; Map: {}, List: {}", size, size_map, size_list);
+        size
+    }
+
+}
+
+
+#[cfg(test)]
+mod test {
+
+    use std::thread::sleep;
+    use std::time::Duration;
+
+    fn inserts(cache: &mut super::CacheDS<String, String>) {
+        cache.insert_without_expiry("a".to_string(), "aa".to_string());
+        cache.insert_with_ttl("b".to_string(), "bb".to_string(), 100);
+    }
+
+    #[test]
+    fn test_inserts() {
+        let mut cache = super::CacheDS::<String, String>::new();
+        inserts(&mut cache);
+        let a = "a".to_string();
+        let b = "b".to_string();
+        assert!(cache.contains_key(&a));
+        assert!(cache.contains_key(&b));
+        assert_eq!(cache.get(&a), Some(&"aa".to_string()));
+    }
+
+    #[test]
+    fn test_removes() {
+        let mut cache = super::CacheDS::<String, String>::new();
+        inserts(&mut cache);
+        let key = "a".to_string();
+        cache.remove(&key);
+        cache.insert_without_expiry(key.clone(), key.clone());
+        cache.remove(&key);
+        assert!(!cache.contains_key(&key));
+    }
+
+    #[test]
+    fn test_get() {
+        let mut cache = super::CacheDS::<String, String>::new();
+        inserts(&mut cache);
+        let a = "a".to_string();
+        let val = "aa".to_string();
+        assert_eq!(cache.get(&a), Some(&val));
+        assert_eq!(cache.get(&a), Some(&val));
+        sleep(Duration::from_millis(110));
+        assert_eq!(cache.get(&("b".to_string())), None);
+        cache.remove(&a);
+        assert_eq!(cache.get(&a), None);
+    }
+
+    #[test]
+    fn test_size() {
+        let mut cache = super::CacheDS::<String, String>::new();
+        let size_a = cache.get_mem_size();
+        inserts(&mut cache);
+        let size_b = cache.get_mem_size();
+        assert!(size_a < size_b);
+    }
+
+}
--- cache/src/cache_ds.rs
+++ cache/src/cache_ds.rs
@@ -1,51 +1,78 @@
 /// heavily inspired by Crate lru_time_cache
 
+extern crate slab;
 extern crate time;
 
 use std::collections::{HashMap, HashSet, VecDeque};
 use std::hash::Hash;
 use time::{Duration, get_time, Timespec};
 
-pub struct CacheDS<K, V> {
-    map: HashMap<K, (V, Option<Timespec>)>,
-    list: VecDeque<K>,
+use self::slab::{Slab};
+
+/// A data structure for a cache with a specified capacity, implementing the LRU eviction strategy.
+/// Values can be inserted with an absolute expiration time, a relative time-to-live or without
+/// expiration. The current implementation uses a slab for outsourcing the values. Applying the
+/// cache to SGX will use unsecured memory instead.
+/// The actual memory (EPC) size needs to be calculated by the admin.
+/// growable size in EPC: capacity * (2*size_of_val(K)[list, Map] + 8[slab indexes] + max_size_of(V)] + 8[Map:usize] + 24[Option<Timespec>])
+// max size_of(V): e. g. Vec<u8>: 24[Vec] + max_size * 8
+pub struct CacheDS<K, V> { // 136 bytes
+    capacity: usize,    // 8 bytes
+    map: HashMap<K, (usize, Option<Timespec>)>, // 40 bytes
+    list: VecDeque<K>, // 40
+    slab: Slab<V, usize>, // 48
 }
 
 impl<K, V> CacheDS<K, V> where K: PartialOrd + Ord + Clone + Hash {
-    pub fn new() -> CacheDS<K, V> {
+    pub fn new(capacity: usize) -> CacheDS<K, V> {
         CacheDS {
-            map: HashMap::new(),
+            capacity: capacity,
+            map: HashMap::with_capacity(capacity),
             list: VecDeque::new(),
+            slab: Slab::new(capacity),
         }
     }
 
-    fn insert(&mut self, key: K, value: V, expiry: Option<Timespec>) -> Option<V> {
-        let result = self.remove(&key);
+    fn insert(&mut self, key: K, value: V, expiry: Option<Timespec>) -> Result<(), &str> {
+        self.remove(&key);
+
         // TODO delete expired values, check for size and in case remove with lru
-        self.map.insert(key.clone(), (value, expiry));
-        self.list.push_back(key);
-        result
+        self.remove_expired();
+        // while not_enough_space {
+        if self.capacity == self.map.len() {
+            self.remove_lru();
+        }
+
+        match self.slab.insert(value) {
+            Ok(idx) => { self.map.insert(key.clone(), (idx, expiry));
+                            self.list.push_back(key);
+                            Ok(())},
+            Err(_) => Err("Error while storing value."),
+        }
 
     }
 
-    pub fn insert_with_expiry(&mut self, key: K, value: V, expiry: Timespec) -> Option<V> {
+    pub fn insert_with_expiry(&mut self, key: K, value: V, expiry: Timespec) -> Result<(), &str> {
         self.insert(key, value, Some(expiry))
     }
 
-    pub fn insert_with_ttl(&mut self, key: K, value: V, ttl: i64) -> Option<V> {
+    pub fn insert_with_ttl(&mut self, key: K, value: V, ttl: i64) -> Result<(), &str> {
         let expiry = get_time() + Duration::milliseconds(ttl);
         self.insert(key, value, Some(expiry))
     }
 
-    pub fn insert_without_expiry(&mut self, key: K, value: V) -> Option<V> {
+    pub fn insert_without_expiry(&mut self, key: K, value: V) -> Result<(), &str> {
         self.insert(key, value, None)
     }
 
     pub fn remove(&mut self, key: &K) -> Option<V> {
         if self.contains_key(key) {
             // self.list.retain(|k| k < key || k > key); // iterating all elements is necassary when keys can be contained multiple times
-            Self::find_and_remove(&mut self.list, key);
-            self.map.remove(key).map(|(value, _)| value)
+            Self::remove_from_list(&mut self.list, key);
+            match self.map.remove(&key) {
+                Some((idx, _)) => { self.slab.remove(idx) },
+                None => None,
+            }
         } else {
             None
         }
@@ -55,15 +82,15 @@ impl<K, V> CacheDS<K, V> where K: PartialOrd + Ord + Clone + Hash {
         self.map.contains_key(key)
     }
 
-    fn find_and_remove(list: &mut VecDeque<K>, key: &K) {
+    fn remove_from_list(list: &mut VecDeque<K>, key: &K) {
         match list.iter().position(|k| !(k < key || k > key)) {
             Some(index) => { list.remove(index); },
             None => { },
         }
     }
 
-    fn is_expired(value: &(V, Option<Timespec>), now: Timespec) -> bool {
-        match value.1 {
+    fn is_expired(value: Option<Timespec>, now: Timespec) -> bool {
+        match value {
             None => false,
             Some(expiry) => expiry <= now,
         }
@@ -73,10 +100,10 @@ impl<K, V> CacheDS<K, V> where K: PartialOrd + Ord + Clone + Hash {
     pub fn get(&mut self, key: &K) -> Option<&V> {
         let result = self.map.get(key);
         match result {
-            Some(x) if !Self::is_expired(x, get_time()) => {
-                Self::find_and_remove(&mut self.list, key);
+            Some(x) if !Self::is_expired(x.1, get_time()) => {
+                Self::remove_from_list(&mut self.list, key);
                 self.list.push_back(key.clone());
-                Some(&x.0)
+                self.slab.get(x.0)
             },
             _ => None,
         }
@@ -85,7 +112,10 @@ impl<K, V> CacheDS<K, V> where K: PartialOrd + Ord + Clone + Hash {
     fn remove_lru(&mut self) {
         match self.list.pop_front() {
             Some(key) => {
-                self.map.remove(&key);
+                match self.map.remove(&key) {
+                    Some((idx, _)) => { self.slab.remove(idx); }
+                    None => {},
+                }
             },
             None => {},
         }
@@ -95,12 +125,17 @@ impl<K, V> CacheDS<K, V> where K: PartialOrd + Ord + Clone + Hash {
         let now = get_time();
         let mut expired = HashSet::new();
         for (k, v) in self.map.iter() {
-            if Self::is_expired(v, now) { expired.insert(k.clone()); }
+            if Self::is_expired(v.1, now) { expired.insert(k.clone()); }
         }
         for key in expired.clone() {
-            self.map.remove(&key);
+            match self.map.remove(&key) {
+                Some((idx, _)) => { self.slab.remove(idx); }
+                None => {},
+            }
         }
+        // self.map.shrink_to_fit();
         self.list.retain(|k| !expired.contains(k));
+        // self.list.shrink_to_fit();
     }
 
 }
@@ -110,47 +145,76 @@ impl<K, V> CacheDS<K, V> where K: PartialOrd + Ord + Clone + Hash {
 mod test {
 
     use std::thread::sleep;
-    use std::time::Duration;
+    use std::time::{Duration};
+    use std::mem::{size_of, size_of_val};
+    use super::slab::Slab;
+    use super::time::Timespec;
 
-    fn inserts(cache: &mut super::CacheDS<String, String>) {
-        cache.insert_without_expiry("a".to_string(), "aa".to_string());
-        cache.insert_with_ttl("b".to_string(), "bb".to_string(), 100);
+    fn inserts(cache: &mut super::CacheDS<String, Vec<u8>>) {
+        cache.insert_without_expiry("a".to_string(), "aa".to_string().into_bytes());
+        cache.insert_with_ttl("b".to_string(), "bb".to_string().into_bytes(), 100);
     }
+
+
+    // #[test]
+    // fn test_size() {
+    //     let mut cache = super::CacheDS::<String, Vec<u8>>::new(2);
+    //     inserts(&mut cache);
+    //     let val = size_of_val(&cache);
+    //     let t = size_of::<Option<Timespec>>();
+    //     println!("size of CacheDS: {}", val);
+    //     assert_eq!(val, 0);
+    // }
 
     #[test]
     fn test_inserts() {
-        let mut cache = super::CacheDS::<String, String>::new();
+        let mut cache = super::CacheDS::<String, Vec<u8>>::new(2);
         inserts(&mut cache);
         let a = "a".to_string();
         let b = "b".to_string();
         assert!(cache.contains_key(&a));
         assert!(cache.contains_key(&b));
-        assert_eq!(cache.get(&a), Some(&"aa".to_string()));
+        assert_eq!(cache.get(&a), Some(&"aa".to_string().into_bytes()));
+
+        assert_eq!(cache.map.len(), cache.list.len());
+        assert_eq!(cache.map.len(), cache.slab.count());
     }
+
+    // #[test]
+    // #[should_panic]
+    // fn test_insert_too_much() {
+    //     let mut cache = super::CacheDS::<String, Vec<u8>>::new(1);
+    //     inserts(&mut cache);
+    // }
 
     #[test]
     fn test_removes() {
-        let mut cache = super::CacheDS::<String, String>::new();
+        let mut cache = super::CacheDS::<String, Vec<u8>>::new(2);
         inserts(&mut cache);
         let key = "a".to_string();
         cache.remove(&key);
-        cache.insert_without_expiry(key.clone(), key.clone());
+        cache.insert_without_expiry(key.clone(), key.clone().into_bytes());
         cache.remove(&key);
         assert!(!cache.contains_key(&key));
+        assert_eq!(cache.map.len(), cache.list.len());
+        assert_eq!(cache.map.len(), cache.slab.count());
+
     }
 
     #[test]
     fn test_get() {
-        let mut cache = super::CacheDS::<String, String>::new();
+        let mut cache = super::CacheDS::<String, Vec<u8>>::new(2);
         inserts(&mut cache);
         let a = "a".to_string();
-        let val = "aa".to_string();
+        let val = "aa".to_string().into_bytes();
         assert_eq!(cache.get(&a), Some(&val));
         assert_eq!(cache.get(&a), Some(&val));
         sleep(Duration::from_millis(110));
         assert_eq!(cache.get(&("b".to_string())), None);
         cache.remove(&a);
         assert_eq!(cache.get(&a), None);
+        assert_eq!(cache.map.len(), cache.list.len());
+        assert_eq!(cache.map.len(), cache.slab.count());
     }
 
 }
