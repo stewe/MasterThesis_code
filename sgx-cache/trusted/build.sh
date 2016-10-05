29e3fe52e48117f262b51cdda2c9df3d782822eb
Stefan Wendt <stefanwendt@posteo.de>
2016-10-05 18:08:30 +0200

    adding private.pem to enclave

--- sgx-cache/trusted/build.sh
+++ sgx-cache/trusted/build.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-cd /home/stefan/playground/first-steps/trusted/
+path="$(pwd)/`dirname '$BASH_SOURCE'`"
+echo $path
+cd $path
+if [ ! -e "target/debug/private.pem" ]; then
+    mkdir -p target && mkdir -p target/debug
+    cp private.pem target/debug/
+fi
+
 cargo build-enclave --verbose -H 0x1000000 -S 0x10000 -- --features "enclave_debug" && cd ./target/debug && sgxs-sign --key private.pem -d trusted.sgxs trusted.sig
 # cargo build-enclave --verbose -H 0x100000 -S 0x10000 -- --features "json enclave_debug" && cd ./target/debug && sgxs-sign --key private.pem -d trusted.sgxs trusted.sig
 # cargo build-enclave --verbose -H 0x100000 -S 0x10000  && cd ./target/debug && sgxs-sign --key private.pem -d trusted.sgxs trusted.sig
