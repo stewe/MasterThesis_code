b0709d191f8f25a16aa0ee74bb11d030e3d3222e
Stefan Wendt <stefanwendt@posteo.de>
2016-10-09 16:10:59 +0200

    enclave build.sh: heap and stack size as parameter

--- sgx-cache/trusted/build.sh
+++ sgx-cache/trusted/build.sh
@@ -3,6 +3,8 @@ scriptdir=`dirname "$BASH_SOURCE"`
 cd $scriptdir
 
 debug=n
+heap=0x1000000	# enclave heap size in Byte (hex)
+stack=0x10000	# enclave stack size in Byte (hex)
 
 for var in "$@"
   do
@@ -21,14 +23,14 @@ if [ $debug = "y" ]; then
     if [ ! -e "target/debug/private.pem" ]; then
         mkdir -p target && mkdir -p target/debug
         cp private.pem target/debug/
-        cargo build-enclave --verbose -H 0x1000000 -S 0x10000 -- --features "enclave_debug" && cd ./target/debug && sgxs-sign --key private.pem -d $name.sgxs $name.sig
+        cargo build-enclave --verbose -H $heap -S $stack -- --features "enclave_debug" && cd ./target/debug && sgxs-sign --key private.pem -d $name.sgxs $name.sig
         # cargo build-enclave --verbose -H 0x100000 -S 0x10000 -- --features "json enclave_debug" && cd ./target/debug && sgxs-sign --key private.pem -d $name.sgxs $name.sig
     fi
 else
     if [ ! -e "target/release/private.pem" ]; then
         mkdir -p target && mkdir -p target/release
         cp private.pem target/release/
-        cargo build-enclave -H 0x1000000 -S 0x10000 -- --release && cd ./target/release && sgxs-sign --key private.pem -d $name.sgxs $name.sig
+        cargo build-enclave -H $heap -S $stack -- --release && cd ./target/release && sgxs-sign --key private.pem -d $name.sgxs $name.sig
     fi
 fi
 
